c57eed9f6d7a21ac56653bea67d9a34f10fbc7fd
Anya Zeng <anya.tseng@gmail.com>
2024-06-19 10:45:11 +1000

    feat: add student.model

---
+++ src/models/student.model.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+
+const studentSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = model("Student", studentSchema);
+//Model name: "Student"
+//In database it would appear "students"，in lowercase and plural
+// NOTE: Schema: Defines the structure of your documents and the data types of their fields.
+// Model: Uses the Schema to create and manage documents in a specific collection
