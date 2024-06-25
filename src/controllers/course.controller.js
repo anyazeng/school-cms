5865bd6348755d012e2931678708319f55786aab
Anya Zeng <anya.tseng@gmail.com>
2024-06-25 17:50:45 +1000

    feat: add error handeling for controllers with trycatch

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -1,53 +1,99 @@
 const CourseModel = require("../models/course.model");
+const createLogger = require("../utils/logger");
+const logger = createLogger(__filename);
+/**
+ * Error handling
+ * 1.callback
+ *  CourseModel.find().exec((err, courses)=>{
+ * if (err) {
+ * res.status(500).json({error:"xxx"})
+ * }
+ * //handle success response
+ * })
+ *
+ * 2.promise
+ *     CourseModel.find().exec.then((coursses)=?{}).catch((err)=>res.status(500)...)
+ * 3.async await
+ *     try {
+ *     const courses = await Course.Model.find().exec();
+ *     } catch (e) {
+ *      res.status(500)
+ *      }
+ */
 
 const getAllCourses = async (req, res, next) => {
-  const page = parseInt(req.query.page) || 1;
-  const pageSize = parseInt(req.query.pageSize) || 2;
-  const skip = (page - 1) * pageSize;
-  const courses = await CourseModel.find().limit(pageSize).skip(skip).exec();
-  res.formatResponse(courses);
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 2;
+    const skip = (page - 1) * pageSize;
+    const courses = await CourseModel.find().limit(pageSize).skip(skip).exec();
+    res.formatResponse(courses);
+  } catch (e) {
+    logger.info(e.message);
+    next(e);
+  }
 };
 
 const getCourseById = async (req, res, next) => {
-  const { id } = req.params;
-  const course = await CourseModel.findById(id).exec();
-  if (!course) {
-    res.formatResponse("Course not found", 404);
-    return;
+  try {
+    const { id } = req.params;
+    const course = await CourseModel.findById(id).exec();
+    if (!course) {
+      res.formatResponse("Course not found", 404);
+      return;
+    }
+    res.formatResponse(course);
+  } catch (e) {
+    logger.info(e.message);
+    next(e);
   }
-  res.formatResponse(course);
 };
 
 const addCourse = async (req, res, next) => {
-  const { code, name, description } = req.body;
-  const course = new CourseModel({ code, name, description });
-  await course.save(); //TODO:need to error handeller
-  res.formatResponse(course, 201);
+  try {
+    const { code, name, description } = req.body;
+    const course = new CourseModel({ code, name, description });
+    await course.save(); //TODO:need to error handeller
+    res.formatResponse(course, 201);
+  } catch (e) {
+    logger.info(e.message);
+    next(e);
+  }
 };
 
 const updateCourseById = async (req, res, next) => {
-  const { id } = req.params;
-  const { name, description } = req.body;
-  const course = await CourseModel.findByIdAndUpdate(
-    id,
-    { name, description },
-    { new: true }
-  ).exec();
-  if (!course) {
-    res.formatResponse("Course not found", 404);
-    return;
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
+    const course = await CourseModel.findByIdAndUpdate(
+      id,
+      { name, description },
+      { new: true }
+    ).exec();
+    if (!course) {
+      res.formatResponse("Course not found", 404);
+      return;
+    }
+    res.formatResponse(course);
+  } catch (e) {
+    logger.info(e.message);
+    next(e);
   }
-  res.formatResponse(course);
 };
 
 const deleteCourseById = async (req, res, next) => {
-  const { id } = req.params;
-  const course = await CourseModel.findByIdAndDelete(id).exec();
-  if (!course) {
-    res.formatResponse("Course not found", 404);
-    return;
+  try {
+    const { id } = req.params;
+    const course = await CourseModel.findByIdAndDelete(id).exec();
+    if (!course) {
+      res.formatResponse("Course not found", 404);
+      return;
+    }
+    res.formatResponse(course, 204);
+  } catch (e) {
+    logger.info(e.message);
+    next(e);
   }
-  res.formatResponse(course, 204);
 };
 
 module.exports = {
--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -3,14 +3,22 @@ const studentRouter = require("../routes/student.router");
 
 //MONGODB commend line: db.collection.fine()
 const getAllStudents = async (req, res, next) => {
-  // NOTE:PAGINATION
-  //if no page, parseInt(undifined)->NAN, NAN->false
-  const page = parseInt(req.query.page) || 1;
-  const pageSize = parseInt(req.query.pageSize) || 2;
-  const skip = (page - 1) * pageSize;
-  const students = await StudentModel.find().limit(pageSize).skip(skip).exec();
-  //NOTE: functions chaining: why? as every function would modify the object but at the same time return a new object
-  res.formatResponse(students);
+  try {
+    // NOTE:PAGINATION
+    //if no page, parseInt(undifined)->NAN, NAN->false
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 2;
+    const skip = (page - 1) * pageSize;
+    const students = await StudentModel.find()
+      .limit(pageSize)
+      .skip(skip)
+      .exec();
+    //NOTE: functions chaining: why? as every function would modify the object but at the same time return a new object
+    res.formatResponse(students);
+  } catch (e) {
+    logger.info(e.message);
+    next(e);
+  }
   //traditional:
   // res.formatResponse(students, 200, {
   //   pagination: { total: await StudentModel.countDocuments() },
@@ -23,49 +31,69 @@ const getAllStudents = async (req, res, next) => {
 
 //MONGODB commend line: find({_id:xxx})
 const getStudentById = async (req, res, next) => {
-  const { id } = req.params;
-  const student = await StudentModel.findById(id).exec();
-  if (!student) {
-    res.formatResponse("Student not found", 404);
-    return;
-    //   NOTE: rmb to add return
+  try {
+    const { id } = req.params;
+    const student = await StudentModel.findById(id).exec();
+    if (!student) {
+      res.formatResponse("Student not found", 404);
+      return;
+      //   NOTE: rmb to add return
+    }
+    res.formatResponse(student);
+  } catch (e) {
+    logger.info(e.message);
+    next(e);
   }
-  res.formatResponse(student);
 };
 
 const updateStudentById = async (req, res, next) => {
-  const { id } = req.params;
-  //TODO: data validtation
-  const student = await StudentModel.findByIdAndUpdate(
-    id,
-    { firstName, lastName, email },
-    { new: true } //NOTE:
-  ).exec();
-  if (!student) {
-    res.formatResponse("Student not found", 404);
-    return;
+  try {
+    const { id } = req.params;
+    //TODO: data validtation
+    const student = await StudentModel.findByIdAndUpdate(
+      id,
+      { firstName, lastName, email },
+      { new: true } //NOTE:
+    ).exec();
+    if (!student) {
+      res.formatResponse("Student not found", 404);
+      return;
+    }
+    res.formatResponse(student);
+  } catch (e) {
+    logger.info(e.message);
+    next(e);
   }
-  res.formatResponse(student);
 };
 
 const addStudent = async (req, res, next) => {
-  const { firstName, lastName, email } = req.body;
-  const student = new StudentModel({ firstName, lastName, email });
-  await student.save();
-  res.formatResponse(student, 201);
+  try {
+    const { firstName, lastName, email } = req.body;
+    const student = new StudentModel({ firstName, lastName, email });
+    await student.save();
+    res.formatResponse(student, 201);
+  } catch (e) {
+    logger.info(e.message);
+    next(e);
+  }
   //student is an object for StudentModel, a document
   //save is a document method, it will trigger validation
   //NOTE:no need use .exec()
 };
 
 const deleteStudentById = async (req, res, next) => {
-  const { id } = req.params;
-  const student = await StudentModel.findByIdAndDelete(id).exec();
-  if (!student) {
-    res.formatResponse("Student not found", 404);
-    return;
+  try {
+    const { id } = req.params;
+    const student = await StudentModel.findByIdAndDelete(id).exec();
+    if (!student) {
+      res.formatResponse("Student not found", 404);
+      return;
+    }
+    res.formatResponse(student, 204);
+  } catch (e) {
+    logger.info(e.message);
+    next(e);
   }
-  res.formatResponse(student, 204);
 };
 
 module.exports = {
